4c748ff4942510001b00c4e3b67a4eb909bfccc1
Ethan Kramer <ethankramer@ethans-air.lan>
2020-10-15 22:47:20 -0400

    add about me section

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -4,6 +4,8 @@ const Hero = () => (
   <div className={styles.container}>
     <p className={styles.header}>Hello.</p>
     <p className={styles.body}> I'm a Charlotte, North Carolina based Full Stack Engineer.</p>
+    {/* Two or three sentences */}
+    <p className={styles.about}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis error eligendi quas commodi ad similique. Ipsam dicta quis consequatur itaque, accusantium provident explicabo atque velit delectus vitae perferendis reiciendis laudantium.</p>
   </div>
 )
 
