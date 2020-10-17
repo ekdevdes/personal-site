0aec8eeeaf81b2a2c2e9eca24b66aeaf51c8d926
Ethan Kramer <ethankramer@ethans-air.lan>
2020-10-17 16:33:59 -0400

    update about me description

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -2,10 +2,9 @@ import styles from './styles.module.scss'
 
 const Hero = () => (
   <div className={styles.container}>
-    <p className={styles.header}>Hello.</p>
+    <p className={styles.header}>Hello</p>
     <p className={styles.body}> I'm a Charlotte, North Carolina based Full Stack Engineer.</p>
-    {/* Two or three sentences */}
-    <p className={styles.about}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis error eligendi quas commodi ad similique. Ipsam dicta quis consequatur itaque, accusantium provident explicabo atque velit delectus vitae perferendis reiciendis laudantium.</p>
+    <p className={styles.about}>I have over 10 years of industry experience with a good mix of professional and volunteer.</p>
   </div>
 )
 
