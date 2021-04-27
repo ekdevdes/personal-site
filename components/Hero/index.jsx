6a1a374e61991154d08086d95c924a0080b6eb63
Ethan Kramer <ethankr234@gmail.com>
2021-04-26 22:04:36 -0400

    update copy in hero

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -4,7 +4,7 @@ const Hero = () => (
   <div className={styles.container}>
     <p className={styles.header}>Hello</p>
     <p className={styles.body}> I'm a Charlotte, North Carolina based Full Stack Engineer.</p>
-    <p className={styles.about}>I have over 10 years of industry experience with a good mix of professional and volunteer.</p>
+    <p className={styles.about}>I possess over 10 years of industry experience both through professional and volunteer work.</p>
   </div>
 )
 
